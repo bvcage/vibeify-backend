1211f732b015e0fa643aa27db668c0c9c0280d0c
Bailey Cage <bailey.cage@gmail.com>
2022-10-16 01:37:39 -0400

    add get_defaults & make_defaults

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -2,4 +2,18 @@ class Playlist < ActiveRecord::Base
    belongs_to :user
    has_many :saves
    has_many :songs, through: :saves
+
+   def self.get_defaults
+      Playlist.all.where(vibeify: true)
+   end
+
+   def self.make_defaults
+      songs_list = Song.categorize_songs
+      songs_list.map do |cat, songs|
+         playlist = Playlist.find_or_create_by(name: cat, vibeify: true)
+         if playlist.songs.count < 20 then playlist.songs << songs end
+         playlist
+      end
+   end
+
 end
