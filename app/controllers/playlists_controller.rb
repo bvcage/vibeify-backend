829f7ac9c3d0785b28e66851a83b93943a9e868e
Bailey Cage <bailey.cage@gmail.com>
2022-10-15 21:44:17 -0400

    add /playlists/merge endpoint

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -26,6 +26,40 @@ class PlaylistsController < ApplicationController
       return_ary.to_json
    end
 
+   post "/playlists/merge" do
+      api = JSON.parse(request.body.read)
+      # get user
+      user_id = api["user"]
+      if user_id.is_a? String
+         user = User.find_by(spotify_id: user_id)
+      elsif user_id.is_a? Integer
+         user = User.find_by(id: user_id)
+      end
+      # create merge playlist
+      list = api["data"]
+      merge = list.map { |item| Playlist.find(item)}
+      name = "Merge of " + merge.pluck(:name).join(' & ')
+      playlist = Playlist.create(
+         name: name,
+         user_id: user.id,
+         owner_id: user.spotify_id
+      )
+      # create joins
+      songs = merge.map do |item|
+         item.songs.each do |song|
+            Save.find_or_create_by(
+               playlist_id: playlist.id,
+               song_id: song.id
+            ) do |s|
+               s.song_spotify_id = song.spotify_id
+            end
+            song
+         end
+         item.songs
+      end
+      { **playlist.attributes, songs: songs.flatten }.to_json
+   end
+
    delete "/playlists" do
       Playlist.destroy_all
       render status: 204
