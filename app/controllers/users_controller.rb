738f300780759ac64be2fda45c162f8a653f6c96
Bailey Cage <bailey.cage@gmail.com>
2022-10-15 21:46:57 -0400

    update /users endpoint; add /users/:id/playlists endpoint

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,15 +6,23 @@ class UsersController < ApplicationController
 
    post "/users" do
       api = JSON.parse(request.body.read)
-      user = User.find_or_create_by(spotify_id: api["id"]) do |user|
-         user.username = api["display_name"]
+      data = api["data"]
+      user = User.find_or_create_by(spotify_id: data["id"]) do |user|
+         user.username = data["display_name"]
       end
       user.to_json
    end
 
    get "/users/:id" do
-      user = User.find_by(spotify_id: params[:id])
+      user = User.find_by(id: params[:id])
       render json: user, status: :ok
    end
-   
+
+   get "/users/:id/playlists" do
+      user = User.find_by!(id: params[:id])
+      user.playlists.to_json
+   rescue ActiveRecord::RecordNotFound
+      { 'error': 'no user found' }
+   end
+
 end
